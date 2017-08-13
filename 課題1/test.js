87c72dda786bb085d2a468497eb060b49ea8e9e5
mizo <ryu5122@gmail.com>
2017-08-13 16:28:34 +0900

    ジャンルのメニュー部分にclick js追加。

---
+++ 課題1/test.js
@@ -0,0 +1,18 @@
+$(function(){
+    $(".photo_btn1").on("click", function(){
+      $(".photo_img1").toggleClass("check");
+    });  
+    $(".photo_btn2").on("click", function(){
+      $(".photo_img2").toggleClass("check");
+    });
+    $(".photo_btn3").on("click", function(){
+      $(".photo_img3").toggleClass("check");
+    });
+    $(".photo_btn4").on("click", function(){
+      $(".photo_img4").toggleClass("check");
+    });
+    $(".photo_btn5").on("click", function(){
+      $(".photo_img5").toggleClass("check");
+    });
+    
+}); 
